146496c7ef71bfc7799becfea3fed2ea539a8f4c
Shayaan-hesabe <shayaansurti@hesabe.com>
2025-04-24 15:30:41 +0300

    changes 1.5

--- Hesabe/Model/Hesabepayment.php
+++ Hesabe/Model/Hesabepayment.php
@@ -1,12 +1,21 @@
 <?php
 namespace HesabePayment\Hesabe\Model;
 
+use Magento\Framework\Model\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\Api\ExtensionAttributesFactory;
+use Magento\Framework\Api\AttributeValueFactory;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Payment\Helper\Data as PaymentHelper;
+use Magento\Payment\Model\Method\Logger;
 use Magento\Payment\Model\Method\AbstractMethod;
 use Magento\Framework\UrlInterface;
 
 class Hesabepayment extends AbstractMethod
 {
     const METHOD_CODE = 'hesabepayment_mpgs';
+
     protected $_code              = self::METHOD_CODE;
     protected $_isOffline         = true;
     protected $_canUseCheckout    = true;
@@ -14,21 +23,23 @@ class Hesabepayment extends AbstractMethod
     private UrlInterface $urlBuilder;
 
     public function __construct(
-        \Magento\Framework\Model\Context                      $context,
-        \Magento\Framework\Registry                           $registry,
-        \Magento\Framework\Api\ExtensionAttributesFactory      $extensionFactory,
-        \Magento\Framework\Api\AttributeValueFactory           $customAttributeFactory,
-        \Magento\Payment\Model\Method\Logger                   $logger,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb          $resourceCollection = null,
-        UrlInterface                                          $urlBuilder,
-        array                                                 $data = []
+        Context $context,
+        Registry $registry,
+        ExtensionAttributesFactory $extensionFactory,
+        AttributeValueFactory $customAttributeFactory,
+        PaymentHelper $paymentData,
+        Logger $logger,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        UrlInterface $urlBuilder,
+        array $data = []
     ) {
         parent::__construct(
             $context,
             $registry,
             $extensionFactory,
             $customAttributeFactory,
+            $paymentData,
             $logger,
             $resource,
             $resourceCollection,
@@ -37,6 +48,9 @@ class Hesabepayment extends AbstractMethod
         $this->urlBuilder = $urlBuilder;
     }
 
+    /**
+     * Redirect URL after placeOrder()
+     */
     public function getOrderPlaceRedirectUrl(): string
     {
         return $this->urlBuilder->getUrl('hesabe/payment/redirect');
