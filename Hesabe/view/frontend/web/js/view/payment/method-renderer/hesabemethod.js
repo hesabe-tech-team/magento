3962f3ab7996f80b0f5e4e5e3650ed59ac7f8e71
Shayaan-hesabe <shayaansurti@hesabe.com>
2025-04-24 10:04:59 +0300

    changes 1.7

--- Hesabe/view/frontend/web/js/view/payment/method-renderer/hesabemethod.js
+++ Hesabe/view/frontend/web/js/view/payment/method-renderer/hesabemethod.js
@@ -1,36 +1,50 @@
-define(
-    [
-        'Magento_Checkout/js/view/payment/default',
-        /*'Magento_Payment/js/view/payment/cc-form',*/
-        'jquery',                
-        'mage/url',
-        'Magento_Checkout/js/model/payment/additional-validators',
-        'Magento_Checkout/js/action/redirect-on-success',
-        'mage/mage',
-        'Magento_Checkout/js/action/place-order',
-        'Magento_Checkout/js/model/full-screen-loader'/*,
-        'Magento_Payment/js/model/credit-card-validation/validator'*/
-    ],
-    function (Component, $, /*data,*/ url, additionalValidators, redirectOnSuccessAction) {
-        'use strict';
+define([
+    'Magento_Checkout/js/view/payment/default',
+    'Magento_Checkout/js/action/place-order',
+    'Magento_Checkout/js/model/payment/additional-validators',
+    'Magento_Checkout/js/action/redirect-on-success',
+    'Magento_Checkout/js/model/full-screen-loader'
+], function (
+    Component,
+    placeOrderAction,
+    additionalValidators,
+    redirectOnSuccessAction,
+    fullScreenLoader
+) {
+    'use strict';
 
-        return Component.extend({
-            defaults: {
-                template: 'HesabePayment_Hesabe/payment/hesabe'
-            },
+    return Component.extend({
+        defaults: {
+            template: 'HesabePayment_Hesabe/payment/hesabe'
+        },
 
-            getCode: function() {
-                return 'hesabe';
-            },
+        getCode: function () {
+            return 'hesabepayment_knet'; // or 'hesabepayment_mpgs'
+        },
 
-            isActive: function() {
-                return true;
-            },
+        isActive: function () {
+            return true;
+        },
 
-            /** Returns payment method instructions */
-            getInstructions: function() {
-                return window.checkoutConfig.payment.instructions[this.item.method];
+        /**
+         * Called when "Place Order" is clicked
+         */
+        placeOrder: function (data, event) {
+            if (event) {
+                event.preventDefault();
             }
-        });
-    }
-);
+
+            if (this.validate() && additionalValidators.validate()) {
+                fullScreenLoader.startLoader();
+                placeOrderAction(this.getData(), this.messageContainer).done(function () {
+                    redirectOnSuccessAction.execute(); // redirects to hesabe/payment/redirect
+                }).fail(function () {
+                    fullScreenLoader.stopLoader();
+                });
+                return true;
+            }
+
+            return false;
+        }
+    });
+});
--- Hesabe/view/frontend/web/js/view/payment/method-renderer/mpgsmethod.js
+++ Hesabe/view/frontend/web/js/view/payment/method-renderer/mpgsmethod.js
@@ -1,34 +1,64 @@
-define(
-    [
-        'Magento_Checkout/js/view/payment/default',
-        'jquery',                
-        'mage/url',
-        'Magento_Checkout/js/model/payment/additional-validators',
-        'Magento_Checkout/js/action/redirect-on-success',
-        'mage/mage',
-        'Magento_Checkout/js/action/place-order',
-        'Magento_Checkout/js/model/full-screen-loader'
-    ],
-    function (Component, $, /*data,*/ url, additionalValidators, redirectOnSuccessAction) {
-        'use strict';
+define([
+    'Magento_Checkout/js/view/payment/default',
+    'jquery',                
+    'mage/url',
+    'Magento_Checkout/js/model/payment/additional-validators',
+    'Magento_Checkout/js/action/redirect-on-success',
+    'mage/mage',
+    'Magento_Checkout/js/action/place-order',
+    'Magento_Checkout/js/model/full-screen-loader'
+], function (
+    Component,
+    $, 
+    url,
+    additionalValidators,
+    redirectOnSuccessAction,
+    mage,
+    placeOrderAction,
+    fullScreenLoader
+) {
+    'use strict';
 
-        return Component.extend({
-            defaults: {
-                template: 'HesabePayment_Hesabe/payment/mpgs'
-            },
+    return Component.extend({
+        defaults: {
+            template: 'HesabePayment_Hesabe/payment/mpgs'
+        },
 
-            getCode: function() {
-                return 'mpgs';
-            },
+        getCode: function () {
+            return 'hesabepayment_mpgs'; // This must match your method code
+        },
 
-            isActive: function() {
-                return true;
-            },
+        isActive: function () {
+            return true;
+        },
 
-            /** Returns payment method instructions */
-            getInstructions: function() {
-                return window.checkoutConfig.payment.instructions[this.item.method];
+        getInstructions: function () {
+            return window.checkoutConfig.payment.instructions
+                ? window.checkoutConfig.payment.instructions[this.item.method]
+                : '';
+        },
+
+        /**
+         * Triggered when the Place Order button is clicked
+         */
+        placeOrder: function (data, event) {
+            if (event) {
+                event.preventDefault();
             }
-        });
-    }
-);
+
+            if (this.validate() && additionalValidators.validate()) {
+                fullScreenLoader.startLoader();
+
+                placeOrderAction(this.getData(), this.messageContainer).done(function () {
+                    redirectOnSuccessAction.execute(); // Redirects to /hesabe/payment/redirect
+                }).fail(function () {
+                    fullScreenLoader.stopLoader();
+                });
+
+                return true;
+            }
+
+            return false;
+        }
+    });
+});
