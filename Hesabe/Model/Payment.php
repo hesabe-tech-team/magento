26a3cc3d318998ac07f11c2d8e9586adbadb6006
Shayaan-hesabe <shayaansurti@hesabe.com>
2025-04-27 14:42:03 +0300

    commit 1.9.6.8

--- Hesabe/Model/Payment.php
+++ Hesabe/Model/Payment.php
@@ -2,13 +2,44 @@
 namespace HesabePayment\Hesabe\Model;
 
 use Magento\Payment\Model\Method\AbstractMethod;
+use Magento\Framework\DataObject;
+use Magento\Quote\Api\Data\PaymentInterface;
 
 class Payment extends AbstractMethod
 {
     const METHOD_CODE     = 'hesabepayment_knet';
 
     protected $_code          = self::METHOD_CODE;
-    protected $_isOffline     = true;
+    protected $_isOffline     = false;
     protected $_canUseCheckout = true;
-    protected $_redirectUrl   = 'hesabe/payment/redirect';
+    protected $_canUseInternal = false;
+    protected $_canUseForMultishipping = false;
+    protected $_isInitializeNeeded = true;
+    protected $_canSaveCc = false;
+    protected $_formBlockType = \HesabePayment\Hesabe\Block\Form\Payment::class;
+    protected $_infoBlockType = \HesabePayment\Hesabe\Block\Info\Payment::class;
+
+    /**
+     * @inheritdoc
+     */
+    public function isAvailable(\Magento\Quote\Api\Data\CartInterface $quote = null)
+    {
+        return parent::isAvailable($quote) && $this->getConfigData('active');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function assignData(DataObject $data)
+    {
+        $additionalData = $data->getData(PaymentInterface::KEY_ADDITIONAL_DATA);
+        if (!is_object($additionalData)) {
+            $additionalData = new DataObject($additionalData ?: []);
+        }
+
+        $info = $this->getInfoInstance();
+        $info->setAdditionalInformation('payment_method', $additionalData->getData('hesabe_payment_method'));
+
+        return $this;
+    }
 }
