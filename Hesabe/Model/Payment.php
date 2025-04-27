3a50a97de0a70e219bc149c5247c08c03b71460f
Shayaan-hesabe <shayaansurti@hesabe.com>
2025-04-27 11:32:52 +0300

    changes 1.9.4

--- Hesabe/Model/Payment.php
+++ Hesabe/Model/Payment.php
@@ -2,13 +2,51 @@
 namespace HesabePayment\Hesabe\Model;
 
 use Magento\Payment\Model\Method\AbstractMethod;
+use Magento\Framework\UrlInterface;
+use Magento\Payment\Model\Method\Logger;
+use Magento\Payment\Model\Method\Plugin\Validator as MethodValidator;
 
 class Payment extends AbstractMethod
 {
-    const METHOD_CODE        = 'hesabepayment_knet';
-    protected $_code         = self::METHOD_CODE;
-    protected $_isOffline    = true;
+    const METHOD_CODE = 'hesabepayment_knet';
+
+    protected $_code = self::METHOD_CODE;
+    protected $_isOffline = true;
     protected $_canUseCheckout = true;
+
+    protected $_urlBuilder;
+    protected $_logger;
+    protected $_methodValidator;
+
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        Logger $logger,
+        UrlInterface $urlBuilder,
+        MethodValidator $methodValidator,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $logger,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+
+        $this->_urlBuilder = $urlBuilder;
+        $this->_logger = $logger;
+        $this->_methodValidator = $methodValidator;
+    }
 
     /**
      * After placeOrder(), Magento redirects here.
