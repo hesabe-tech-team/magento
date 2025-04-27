6492a98731a1c94fea03c5efa4425645ffff8f9b
Shayaan-hesabe <shayaansurti@hesabe.com>
2025-04-27 10:23:05 +0300

    changes 1.9.1

--- Hesabe/Model/Hesabepayment.php
+++ Hesabe/Model/Hesabepayment.php
@@ -6,13 +6,12 @@ use Magento\Payment\Model\Method\AbstractMethod;
 class Hesabepayment extends AbstractMethod
 {
     const METHOD_CODE        = 'hesabepayment_mpgs';
-
     protected $_code         = self::METHOD_CODE;
     protected $_isOffline    = true;
     protected $_canUseCheckout = true;
 
     /**
-     * After placeOrder(), Magento will redirect here.
+     * After placeOrder(), Magento redirects here.
      */
     public function getOrderPlaceRedirectUrl(): string
     {
