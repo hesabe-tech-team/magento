ce4f78c795b483ae14831992332970238dea819d
Shayaan-hesabe <shayaansurti@hesabe.com>
2025-04-27 12:17:22 +0300

    changes 1.9.6.1

--- Hesabe/Model/Hesabepayment.php
+++ Hesabe/Model/Hesabepayment.php
@@ -5,6 +5,7 @@ use Magento\Payment\Model\Method\AbstractMethod;
 use Magento\Framework\UrlInterface;
 use Magento\Payment\Model\Method\Logger;
 use Magento\Payment\Gateway\Validator\ValidatorInterface as MethodValidator;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class Hesabepayment extends AbstractMethod
 {
@@ -23,6 +24,7 @@ class Hesabepayment extends AbstractMethod
         \Magento\Framework\Registry $registry,
         \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
         \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        ScopeConfigInterface $scopeConfig,
         \Magento\Payment\Helper\Data $paymentData,
         Logger $logger,
         UrlInterface $urlBuilder,
@@ -36,6 +38,7 @@ class Hesabepayment extends AbstractMethod
             $registry,
             $extensionFactory,
             $customAttributeFactory,
+            $scopeConfig,
             $paymentData,
             $logger,
             $resource,
